0e82bff1bca1b3e1990e574d7a3559d97b907201
Nawaz Rashid <nawazrashid2507@gmail.com>
2026-06-29 18:16:47 +0530

    Added solved leetcode problems

--- LEETCODE PROBLEMS/problem_169.cpp
+++ LEETCODE PROBLEMS/problem_169.cpp
@@ -30,3 +30,36 @@ int main()
 
     return 0;
 }
+
+
+/* ye optimal approach hai iski
+int majorityElement(vector<int>& nums)
+{
+    int freq = 0 , ans = 0;
+    int n = nums.size();
+
+    for(int i = 0; i<n ; i++){
+        if(freq == 0){
+            ans = nums[i];
+        }
+        if(ans == nums[i]){
+            freq++ ; 
+        }
+        else{
+            freq-- ;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    // MAJORITY ELEMENT :- MOORE'S ALGORITHM
+
+    vector<int> nums = {1,2,2,1,1};
+
+    cout << "Majority element in the given vector is: " << majorityElement(nums) << endl;
+
+    return 0;
+}*/
